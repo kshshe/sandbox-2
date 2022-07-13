6343febb8615363ca19952ad55acbae7983c0f16
Алексей Лауд <services@a-laud.ru>
2022-07-13 16:48:46 +0400

    Fix humidity color

--- src/draw.ts
+++ src/draw.ts
@@ -25,7 +25,7 @@ export const redrawPoint = (coordinate: Coordinate) => {
     ctx.fillRect(x * SCALE, y * SCALE, SCALE, SCALE)
   } else {
     const { type } = point
-    ctx.fillStyle = getColor(type, point.temperature, point.type === PointType.Water ? 0 : point.humidity)
+    ctx.fillStyle = getColor(type, point.temperature, point.type === PointType.Sand ? point.humidity : 0)
     ctx.fillRect(x * SCALE, y * SCALE, SCALE, SCALE)
     if (debug) {
       ctx.fillText('', x * SCALE,(y+1) * SCALE)
