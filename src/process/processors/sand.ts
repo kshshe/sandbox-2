8c710244ed0e7cbdf42ad37be3851e358b355b88
Алексей Лауд <services@a-laud.ru>
2022-07-13 13:36:46 +0400

    Add sand diagonal moves

--- src/process/processors/sand.ts
+++ src/process/processors/sand.ts
@@ -1,5 +1,5 @@
 import { Processor, RequestedAction } from '../types'
-import { canMoveDown } from '../utils/canMove'
+import { canMoveDown, canMoveLeftDown, canMoveRightDown } from '../utils/canMove'
 
 export const sandProcessor: Processor = (state, point) => {
   if (point.temperature > 120) {
@@ -8,5 +8,15 @@ export const sandProcessor: Processor = (state, point) => {
   if (canMoveDown(state, point)) {
     return RequestedAction.MoveDown
   }
+
+  const availableActionsFirstPriority = [
+    canMoveLeftDown(state, point) && RequestedAction.MoveLeftDown,
+    canMoveRightDown(state, point) && RequestedAction.MoveRightDown,
+  ].filter(Boolean) as RequestedAction[]
+
+  if (availableActionsFirstPriority.length > 0) {
+    return availableActionsFirstPriority[Math.floor(Math.random() * availableActionsFirstPriority.length)]
+  }
+  
   return RequestedAction.None
 }
