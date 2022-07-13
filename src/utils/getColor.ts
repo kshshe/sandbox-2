bcc80764fe0ab1678aeba0776f700e641d2de18a
Алексей Лауд <services@a-laud.ru>
2022-07-13 15:39:36 +0400

    Fix color

--- src/utils/getColor.ts
+++ src/utils/getColor.ts
@@ -16,6 +16,7 @@ const COLORS: Record<PointType, string> = {
   [PointType.Cold]: '#0078af',
   [PointType.Void]: '#000000',
   [PointType.Clone]: '#00a927',
+  [PointType.NonExistentElement]: '#ffffff',
 }
 
 const COLD = '#0000FF'
