544c7ebe463d69c7c9b95f1d57295c1ff43c0d20
Алексей Лауд <services@a-laud.ru>
2022-07-17 18:37:47 +0400

    Add fire for wood

--- src/process/processors/fireWood.ts
+++ src/process/processors/fireWood.ts
@@ -1,8 +1,14 @@
+import { PointType } from '../../data'
 import { Processor, RequestedAction } from '../types'
+import { cloneProcessor } from './clone'
 
-export const fireWoodProcessor: Processor = (state, point) => {
+export const fireWoodProcessor: Processor = (state, point, tick) => {
   if (point.age > 1000) {
     return RequestedAction.Die
+  }
+
+  if (Math.random() > 0.5) {
+    return cloneProcessor(state, {...point, cloningType: PointType.Fire}, tick)
   }
 
   return RequestedAction.None
