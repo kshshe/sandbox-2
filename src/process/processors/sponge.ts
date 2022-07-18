76ce74e29c39f36cbce7f4e06760fb7b6b4ec3e2
Алексей Лауд <services@a-laud.ru>
2022-07-18 17:21:03 +0400

    Make sponge burn

--- src/process/processors/sponge.ts
+++ src/process/processors/sponge.ts
@@ -2,9 +2,24 @@ import { Processor, RequestedAction } from '../types'
 import { findNeighbours } from '../utils/findNeighbours'
 import { redrawPoint } from '../../draw'
 import { PointType } from '../../data'
+import { cloneProcessor } from './clone'
 
-export const spongeProcessor: Processor = (state, point) => {
+export const spongeProcessor: Processor = (state, point, tick) => {
   const neighbours = findNeighbours(state, point)
+
+  if (point.temperature > 150) {
+    neighbours.forEach(neighbour => {
+      if (neighbour.type === point.type) {
+        neighbour.temperature = 300
+        redrawPoint(neighbour.coordinate)
+      }
+    })
+    if (Math.random() > 0.9) {
+      cloneProcessor(state, {...point, cloningType: PointType.Fire}, tick)
+    }
+    return RequestedAction.Melt
+  }
+
   neighbours.forEach((neighbour) => {
     if (neighbour.type === PointType.Water) {
       if (Math.random() > 0.96) {
