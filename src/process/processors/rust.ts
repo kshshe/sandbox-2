f8d13148ffc3eab28809c8eea5c3ca2b551aeb32
Aleksei Laud <services@a-laud.ru>
2023-03-13 12:07:18 +0400

    Fix rust

--- src/process/processors/rust.ts
+++ src/process/processors/rust.ts
@@ -1,6 +1,14 @@
-import { Processor } from '../types'
+import { PointType } from '../../data'
+import { Processor, RequestedAction } from '../types'
+import { findNeighbours } from '../utils/findNeighbours'
 import { sandProcessor } from './sand'
 
 export const rustProcessor: Processor = (state, point, tick) => {
+  const hasMetalNeighbours = findNeighbours(state, point).some(
+    (p) => p.type === PointType.Metal
+  )
+  if (hasMetalNeighbours) {
+    return RequestedAction.None;
+  }
   return sandProcessor(state, {...point, temperature: 10, humidity: 0}, tick)
 }
